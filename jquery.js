39ef352fa8722ea73ec2f337ec3887b16e4a2c79
Henok Mekonnen <hmekonn1@asu.edu>
2014-11-26 01:17:25 -0800

    Individual assignment/jQuiry pictures and links and tages and html skills
    Demonstrate jQuery capablity to modify webpage, pictures and links and
    tags. I also used html skills and tricks to do simple css

---
+++ jquery.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+	$("image").hide
+	$("h3").css("color","blue")
+	$("p").css("color", "Green")
+	$("h1").click(function() {
+		$(this).next().slideToggle(300);
+
+	});
+	
+	
+	
+});
+
+
+$("p").click(function goToURL(){
+
+location.href="https://sites.google.com/a/asu.edu/henok-eng/";
+
+});
+
